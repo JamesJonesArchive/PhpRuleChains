c999e64a72f329c323287bde62aba5f9e34c286e
James Jones <jamjon3@gmail.com>
2015-10-01 14:17:36 -0400

    Initial push

---
+++ src/Rule.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace CF\RuleChains;
+/**
+ * Description of Rule
+ *
+ * @author James Jones <jamjon3@gmail.com>
+ */
+class Rule {
+    //put your code here
+}
